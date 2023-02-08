c166741823eb454f7140ea217e04b03320a3a225
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2023-02-08 19:41:08 +0530

    commit

--- lib/registration/screens/sign_in_screen.dart
+++ lib/registration/screens/sign_in_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kalakriti/registration/services/auth_service.dart';
+import 'package:kalakriti/registration/utilis/utilis.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -8,8 +10,31 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  //final formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
+  String email = "";
+  String password = "";
+  AuthService authService = AuthService();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size screenSize = utilis().getscreenSize();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 80,
+        ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("lib/assets/logo.png",
+                //height: screenSize.height * 0.25,
+                //width: screenSize.width * 0.70,
+              ),
+            ],
+          ),
+        ),
+      );
   }
 }
